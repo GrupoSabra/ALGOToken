320d0c4de64d7cfe72e431dd37fb3fe1b8cf7184
pmmax <pmmax.net@gmail.com>
2019-03-19 16:52:30 -0300

    Fixed a bug in the miner fee calculation algorithm. Added a unit test for the algorithm.

---
+++ labs/algo-token-distribution/test/AdvancedAlgos.AlgoToken.AlgoTokenDistribution.IntegrationTests/Algorithms/AlgoMinerFeeAlgorithmTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using AdvancedAlgos.AlgoToken.AlgoErc20Token;
+using AdvancedAlgos.AlgoToken.AlgoTokenDistribution.Algorithms;
+using Xunit;
+
+namespace AdvancedAlgos.AlgoToken.AlgoTokenDistribution.IntegrationTests.Algorithms
+{
+    public class AlgoMinerFeeAlgorithmTests
+    {
+        [Fact]
+        public void TestAlgoMinerFeeAlgorithm()
+        {
+            BigInteger categorySupply = 2.MAlgo();
+            BigInteger currentYearSupply = categorySupply / 2;
+            BigInteger totalExpectedFees = 0;
+
+            for (int year = 1; year <= 10; year++)
+            {
+                totalExpectedFees += currentYearSupply;
+
+                AssertInRange(totalExpectedFees, AlgoMinerFeeAlgorithm.CalculateFees(2.MAlgo(), 365 * year), 1);
+
+                currentYearSupply /= 2;
+            }
+        }
+
+        private void AssertInRange(BigInteger expected, BigInteger actual, BigInteger tolerancePercentage)
+        {
+            var tolerance = expected * tolerancePercentage / 100;
+            var lowerBound = expected - tolerance;
+            var upperBound = expected + tolerance;
+
+            Assert.InRange(expected, lowerBound, upperBound);
+        }
+    }
+}
